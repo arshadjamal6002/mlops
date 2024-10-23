bdf0ac0a1d4cc46cae0b093b3b4098477594a2b9
Arshad <arshadjamal6002@gmail.com>
2024-10-23 06:23:26 +0300

    first pipeline using bow

---
+++ src/models/model_building.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import pickle
+import yaml
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+params = yaml.safe_load(open('params.yaml', 'r'))['model_building']
+
+# fetch the data from data/processed
+train_data = pd.read_csv('./data/features/train_bow.csv')
+
+X_train = train_data.iloc[:,0:-1].values
+y_train = train_data.iloc[:,-1].values
+
+# Define and train the XGBoost model
+
+clf = GradientBoostingClassifier(n_estimators=params['n_estimators'], learning_rate = params['learning_rate'])
+clf.fit(X_train, y_train)
+
+# save
+pickle.dump(clf, open('model.pkl','wb'))
+
